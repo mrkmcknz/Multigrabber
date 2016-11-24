6a3b0018540b6f408c63189e72c099343267a4ea
Mark McKenzie <mark.mckenzie@oldstlabs.com>
2016-11-24 16:58:08 +0000

    RSS fetch and store in local Elasticsearch

--- run.py
+++ run.py
@@ -1,18 +1,24 @@
 #!/usr/local/bin/python3.5
+import json
 import argparse
-import aiohttp
 import asyncio
-import feedparser
+import hashlib
 
+import aiohttp
+import arrow
+import feedparser
+from elasticsearch import Elasticsearch
+from elasticsearch import helpers
 
 async def fetch(session, url):
     with aiohttp.Timeout(10):
         async with session.get(url) as response:
             return await response.read()
 
-async def run(filepath):
+async def run(filepath, es):
 
     tasks = []
+    actions = []
 
     async with aiohttp.ClientSession() as session:
         with open(filepath, "r") as input:
@@ -21,9 +27,24 @@ async def run(filepath):
                 tasks.append(task)
 
             data = await asyncio.gather(*tasks)
-            # Do what you want with the data
-            for i in data:
-                parse_rss_feed(i)
+            results = [parse_rss_feed(i) for i in data]
+            for feed in results:
+                actions = es_action(feed, actions)
+            helpers.bulk(es, actions)
+
+
+def es_action(feed, actions):
+
+    for item in feed:
+        action = {
+            "_id": hashlib.md5(item['link'].encode('utf-8')).hexdigest(),
+            "_index": "news",
+            "_type": "feed",
+            "_source": item,
+            "op_type": "create"
+        }
+        actions.append(action)
+    return actions
 
 
 def rss_item(item):
@@ -31,8 +52,8 @@ def rss_item(item):
     data = {
         "title": item.title,
         "link": item.link,
-        "descriptions": item.description,
-        "published_on": item.published
+        "description": item.description,
+        "published_on": arrow.get(item.published_parsed).isoformat()
     }
     return data
 
@@ -44,12 +65,19 @@ def parse_rss_feed(feed):
 
 if __name__ == '__main__':
 
+    # By default we make a connection to: 0.0.0.0:9200
+    es = Elasticsearch()
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '-i', dest="filepath", required=True, metavar="FILE",
         help="path to file containing urls to be processed")
     args = parser.parse_args()
 
+    import time
+    start = time.time()
     loop = asyncio.get_event_loop()
-    future = asyncio.ensure_future(run(args.filepath))
+    future = asyncio.ensure_future(run(args.filepath, es))
     loop.run_until_complete(future)
+    end = time.time()
+    print(end - start)
