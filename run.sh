268aa830304ffa85413b2abb67eeca5ee9e4299c
Mark McKenzie <mark.mckenzie@oldstlabs.com>
2016-12-07 12:16:59 +0000

    for loop on bash script

--- run.sh
+++ run.sh
@@ -2,4 +2,8 @@
 
 rm -f $(ls | grep -i 'input-')
 split -l 200 data/input.txt input-
-parallel python run.py -i ::: input-[a-z][a-z]
+
+for i in input-[a-z][a-z]; do
+    rm $i
+    nohup python run.py -i $i &
+done
